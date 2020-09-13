516208c22a3bfe6afd3d3c28c2d7def38465ab28
Aban Hussain <hsnabn@gmail.com>
2020-09-13 19:51:51 +0500

    modifications to IK, works a bit now also fixed animation root motion for idle

--- Assets/Player/PlayerAnimationControllerLogic.cs
+++ Assets/Player/PlayerAnimationControllerLogic.cs
@@ -82,63 +82,60 @@ public class PlayerAnimationControllerLogic : MonoBehaviour
 
     private void OnAnimatorIK(int layerIndex)
     {
-        DoFootIK(AvatarIKGoal.LeftFoot, leftFoot, animator.GetFloat("LeftFootGround"), out var rFHit);
-        DoFootIK(AvatarIKGoal.RightFoot, rightFoot, animator.GetFloat("RightFootGround"), out var lFHit);
+        DoFootIK(AvatarIKGoal.LeftFoot, leftFoot, animator.leftFeetBottomHeight, animator.GetFloat("LeftFootGround"), out var rFHit);
+        DoFootIK(AvatarIKGoal.RightFoot, rightFoot, animator.rightFeetBottomHeight, animator.GetFloat("RightFootGround"), out var lFHit);
 
-        if (lFHit.point.y <= rFHit.point.y)
-        {
-            fixPosition(lFHit.point);
-        }
-        else
-        {
-            fixPosition(rFHit.point);
-        }
+        var offset = GetOffset(lFHit.point, rFHit.point);
+        fixPosition(offset);
     }
 
-    private void DoFootIK(AvatarIKGoal foot, Transform footTransform, float weight, out RaycastHit hit)
+    private void DoFootIK(AvatarIKGoal foot, Transform footTransform, float footBottomHeight, float weight, out RaycastHit hit)
     {
-        animator.SetIKPositionWeight(foot, weight);
-        animator.SetIKRotationWeight(foot, weight);
-
         Vector3 pos = footTransform.position;
+        var rot = footTransform.rotation;
 
         var ray = new Ray(pos + Vector3.up, Vector3.down);
 
-        if (Physics.Raycast(ray, out hit, 5.0f))
+        var didHit = false;
+
+        if (Physics.Raycast(ray, out hit, 1.5f))
         {
-            if (hit.transform.tag == "Walkable")
+            didHit = true;
+
+            if (hit.transform.CompareTag("Walkable"))
             {
                 Vector3 footPos = hit.point;
-                footPos.y += distanceToGround;
-
-                var rot = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-                animator.SetIKRotation(foot, rot);
+                footPos.y += footBottomHeight;
 
                 Vector3 distance = footPos - animator.GetIKPosition(foot);
 
                 if (distance.sqrMagnitude > 0.015f)
                 {
-                    animator.SetIKPosition(foot, footPos);
-
+                    pos = footPos;
+                    rot = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
                 }
             }
         }
+
+        Debug.DrawRay(ray.origin, ray.direction * 1.5f, didHit ? Color.green : Color.red, 0f, false);
+
+        animator.SetIKPositionWeight(foot, weight);
+        animator.SetIKRotationWeight(foot, weight);
+
+        animator.SetIKPosition(foot, pos);
+        animator.SetIKRotation(foot, rot);
     }
 
-    float posLerp = 0f;
-    void fixPosition(Vector3 pos)
+    float _lastBodyY;
+    void fixPosition(float offset)
     {
-        if(Mathf.Abs(transform.position.y - pos.y) > 0.05f)
-        {
-            if (posLerp > 1.0f)
-            {
-                posLerp = 0f;
-            }
+        var newPos = animator.bodyPosition;
+        newPos.y += offset;
+        newPos.y = Mathf.MoveTowards(_lastBodyY, newPos.y, 5f * Time.deltaTime);
 
-            Vector3 newPos = transform.position;
-            newPos.y = Mathf.Lerp(transform.position.y, pos.y, posLerp);
-            posLerp += 0.05f * Time.deltaTime;
-            transform.position = newPos;
-        }
+        animator.bodyPosition = newPos;
+        _lastBodyY = newPos.y;
     }
+
+    private float GetOffset(Vector3 lfPos, Vector3 rfPos) => Mathf.Min(lfPos.y - transform.position.y - animator.leftFeetBottomHeight, rfPos.y - transform.position.y - animator.rightFeetBottomHeight);
 }
